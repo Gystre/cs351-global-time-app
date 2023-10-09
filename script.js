4080ee33638bb4365c596750334c5efab8da6ab4
Kyle Yu <gystre@gmail.com>
2023-10-09 14:02:15 -0700

    make it look nicer

--- script.js
+++ script.js
@@ -1,35 +1,46 @@
 const timezoneOffsets = {
-    'UTC': 0,
-    'NewYork': -5,
-    'London': 0,
-    'Sydney': 11,
-    'Tokyo': 9
-    // Add more cities and their UTC offsets as needed
-}
+  UTC: 0,
+  NewYork: -5,
+  London: 0,
+  Sydney: 11,
+  Tokyo: 9,
+  // Add more cities and their UTC offsets as needed
+};
 
 function getCityTime(city) {
-    const now = new Date();
-    const utcTime = now.getTime() + now.getTimezoneOffset() * 60000; // Convert local time to UTC time in milliseconds
-    const cityOffset = timezoneOffsets[city] * 60 * 60000; // Convert city UTC offset to milliseconds
-    return new Date(utcTime + cityOffset);
+  const now = new Date();
+  const utcTime = now.getTime() + now.getTimezoneOffset() * 60000; // Convert local time to UTC time in milliseconds
+  const cityOffset = timezoneOffsets[city] * 60 * 60000; // Convert city UTC offset to milliseconds
+  return new Date(utcTime + cityOffset);
+}
+
+// city = string
+function addClockDiv(city) {
+  // container div to store the time and the city name
+  const container = document.createElement("div");
+  container.className = "city-clock";
+
+  // city name
+  const cityNameDiv = document.createElement("div");
+  cityNameDiv.textContent = city;
+
+  // the time
+  const cityTime = getCityTime(city);
+  const timeDisplayDiv = document.createElement("div");
+  timeDisplayDiv.className = "time-display";
+  timeDisplayDiv.textContent = cityTime.toISOString().slice(11, 19); // Display only the HH:MM:SS portion
+
+  container.appendChild(cityNameDiv);
+  container.appendChild(timeDisplayDiv);
+
+  document.getElementById("clocks").appendChild(container);
 }
 
 function addCityTime() {
-    const city = document.getElementById("city-selector").value;
-    const cityTime = getCityTime(city);
-
-    const clockDiv = document.createElement("div");
-    clockDiv.className = "city-clock";
-
-    const cityNameDiv = document.createElement("div");
-    cityNameDiv.textContent = city;
-
-    const timeDisplayDiv = document.createElement("div");
-    timeDisplayDiv.className = "time-display";
-    timeDisplayDiv.textContent = cityTime.toISOString().slice(11, 19); // Display only the HH:MM:SS portion
-
-    clockDiv.appendChild(cityNameDiv);
-    clockDiv.appendChild(timeDisplayDiv);
-    
-    document.getElementById("clocks").appendChild(clockDiv);
+  const city = document.getElementById("city-selector").value;
+  addClockDiv(city);
 }
+
+// switch to a more automated solution instead of hardcoding offsets
+// add an example clock
+// addClockDiv("San Francisco, USA");
